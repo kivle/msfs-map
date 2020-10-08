2e08f105c096583ec29d33da256da776f5bf4aac
Helge Elvik <helge.elvik@gmail.com>
2020-10-08 18:48:58 +0200

    Updates

---
+++ precache-manifest.e3c1b8d4b79eda94bac4085fcb553cfa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f567869b5649aa2bcf0ca58e8bddc95b",
+    "url": "/msfs-map/index.html"
+  },
+  {
+    "revision": "e289e0d3b85f3f1addb3",
+    "url": "/msfs-map/static/css/2.a4567992.chunk.css"
+  },
+  {
+    "revision": "474bad4d01aa0ae877ed",
+    "url": "/msfs-map/static/css/main.bd2ec066.chunk.css"
+  },
+  {
+    "revision": "e289e0d3b85f3f1addb3",
+    "url": "/msfs-map/static/js/2.7bc31ced.chunk.js"
+  },
+  {
+    "revision": "7921a0c81ceb875956bc103e4e33e690",
+    "url": "/msfs-map/static/js/2.7bc31ced.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "474bad4d01aa0ae877ed",
+    "url": "/msfs-map/static/js/main.f60fa093.chunk.js"
+  },
+  {
+    "revision": "7e8058f8e6ead95b0a75",
+    "url": "/msfs-map/static/js/runtime-main.10dfa2d7.js"
+  }
+]);
