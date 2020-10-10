24aa32f9188a1ec29e648290939f0bbcacae0861
Helge Elvik <helge.elvik@gmail.com>
2020-10-10 10:46:26 +0200

    Updates

--- precache-manifest.e7c5b0ff45962f3cb095afe986a6f9a7.js
+++ precache-manifest.e7c5b0ff45962f3cb095afe986a6f9a7.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "cf3d34e1c2292629c604900090c2f240",
+    "revision": "1ea2c539c3e8008fd08fffe900e1ca3b",
     "url": "/msfs-map/index.html"
   },
   {
-    "revision": "f01ae03d630a988a225a",
+    "revision": "142df7f01a2f079e19d0",
     "url": "/msfs-map/static/css/2.a4567992.chunk.css"
   },
   {
-    "revision": "e4f688cfa220eba0a0c6",
+    "revision": "4adac2bdab269c69c215",
     "url": "/msfs-map/static/css/main.41ff5c5b.chunk.css"
   },
   {
-    "revision": "f01ae03d630a988a225a",
-    "url": "/msfs-map/static/js/2.f3dd4f0e.chunk.js"
+    "revision": "142df7f01a2f079e19d0",
+    "url": "/msfs-map/static/js/2.b4a19090.chunk.js"
   },
   {
     "revision": "7921a0c81ceb875956bc103e4e33e690",
-    "url": "/msfs-map/static/js/2.f3dd4f0e.chunk.js.LICENSE.txt"
+    "url": "/msfs-map/static/js/2.b4a19090.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "e4f688cfa220eba0a0c6",
-    "url": "/msfs-map/static/js/main.19066cf2.chunk.js"
+    "revision": "4adac2bdab269c69c215",
+    "url": "/msfs-map/static/js/main.00824676.chunk.js"
   },
   {
     "revision": "7e8058f8e6ead95b0a75",
