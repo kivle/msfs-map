d9176cdbeda26b10c60ffe431d05524317b01d5d
Helge Elvik <helge.elvik@gmail.com>
2021-01-02 19:02:32 +0100

    Add extra map layers

--- src/features/map/layers/MainLayer.js
+++ src/features/map/layers/MainLayer.js
@@ -16,6 +16,7 @@ export const MainLayer = React.memo(({ currentMap }) => {
     mainLayer = <TileLayer
       attribution={attribution}
       url={currentMap.tileServer}
+      subdomains={currentMap?.subdomains}
     />;
   }
   
--- src/features/map/mapSlice.js
+++ src/features/map/mapSlice.js
@@ -53,6 +53,20 @@ export const mapSlice = createSlice({
         type: 'tileServer', 
         tileServer: 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png',
         attribution: `&copy; <a target="_blank' href="https://carto.com">Carto</a>`
+      },
+      {
+        name: 'Carto (voyager)',
+        type: 'tileServer',
+        tileServer: 'https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png',
+        attribution: '&copy; <a target="_blank" href="https://osm.org/copyright">OpenStreetMap</a> <a target="_blank" href="https://carto.com/">Carto</a>',
+        subdomains: 'abcd'
+      },
+      {
+        name: 'Airspace data (OpenAIP)',
+        type: 'tileServer',
+        tileServer: 'https://{s}.tile.maps.openaip.net/geowebcache/service/tms/1.0.0/openaip_basemap@EPSG%3A900913@png/{z}/{x}/{y}.png',
+        attribution: '"Airspace data: <a target="_blank" href="https://www.openaip.net/">OpenAIP</a>',
+        subdomains: '12'
       }
     ]
   },
