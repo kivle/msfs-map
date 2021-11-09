66af768bc46c5193f06f5e6a3c10ad60b1685064
Helge Elvik <helge.elvik@gmail.com>
2021-11-09 23:39:05 +0100

    Clean up wikipedia layer code

--- src/features/map/layers/Wikipedia.js
+++ src/features/map/layers/Wikipedia.js
@@ -1,73 +1,19 @@
 import * as React from "react";
-
-import Marker from "react-leaflet-enhanced-marker";
 import { useSelector } from "react-redux";
-import { FaBookReader, FaWikipediaW } from "react-icons/fa";
-import { MdRecordVoiceOver } from 'react-icons/md';
-
 import { selectCurrentPage, selectIsPlaying, selectPages } from "../../wikipedia/wikipediaSlice";
-
-import styles from './Wikipedia.module.css';
+import WikipediaMarker from "./WikipediaMarker";
 
 export default React.memo(function Wikipedia() {
+  const pages = useSelector(selectPages);
   const currentPage = useSelector(selectCurrentPage);
   const isPlaying = useSelector(selectIsPlaying);
-  const pages = useSelector(selectPages);
 
-  return (
-    <>
-      {pages?.map(
-        (p) =>
-          p?.coordinates?.map(c => {
-            const { pageid, rating } = p;
-            const { lat, lon } = c;
-            let cn = styles.marker;
-            if (pageid === currentPage.pageid) {
-              cn += ` ${styles.current}`;
-            }
-            if (rating === 'bad') {
-              cn += ` ${styles.ratedBad}`;
-            }
-
-            return (
-              <Marker
-                key={pageid}
-                position={[lat, lon]}
-                icon={
-                  <div className={styles.container}>
-                    <div className={cn}>
-                      <WikipediaIcon 
-                        isReading={isPlaying && pageid === currentPage.pageid}
-                        tts={rating === 'good'} />
-                    </div>
-                  </div>
-                }
-              />
-            );
-          })
-      )}
-    </>
+  return pages?.map(p => 
+    <WikipediaMarker 
+      key={p.pageid} 
+      page={p} 
+      isCurrentPage={p.pageid === currentPage?.pageid} 
+      isPlaying={isPlaying} 
+    />
   );
 });
-
-function WikipediaIcon({ isReading, tts }) {
-  return (
-    <>
-      {!isReading && <FaWikipediaW size={32} />}
-      {isReading && tts &&
-        <MdRecordVoiceOver 
-          className={styles.speaker}
-          size={32} 
-          stroke="black"
-          strokeWidth={1}
-          color="#FFF" 
-        />}
-      {isReading && !tts &&
-        <FaBookReader
-          className={styles.speaker}
-          size={30}
-        />
-      }
-    </>
-  );
-};
---
+++ src/features/map/layers/WikipediaMarker.js
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import Marker from "react-leaflet-enhanced-marker";
+import { FaBookReader, FaWikipediaW } from "react-icons/fa";
+import { MdRecordVoiceOver } from 'react-icons/md';
+import styles from './WikipediaMarker.module.css';
+
+function WikipediaIcon({ isReading, tts }) {
+  return (
+    <>
+      {!isReading && <FaWikipediaW size={32} />}
+      {isReading && tts &&
+        <MdRecordVoiceOver 
+          className={styles.speaker}
+          size={32} 
+          stroke="black"
+          strokeWidth={1}
+          color="#FFF" 
+        />}
+      {isReading && !tts &&
+        <FaBookReader
+          className={styles.speaker}
+          size={30}
+        />
+      }
+    </>
+  );
+};
+
+export default React.memo(function WikipediaMarker({
+  page, isCurrentPage, isPlaying
+}) {
+  const { pageid, rating, coordinates } = page;
+  let cn = styles.marker;
+  if (isCurrentPage) {
+    cn += ` ${styles.current}`;
+  }
+  if (rating === 'bad') {
+    cn += ` ${styles.ratedBad}`;
+  }
+
+  return coordinates?.map((c, i) => <Marker
+    key={`${pageid}-${i}`}
+    position={c}
+    icon={
+      <div className={styles.container}>
+        <div className={cn}>
+          <WikipediaIcon 
+            isReading={isPlaying && isCurrentPage}
+            tts={rating === 'good'} />
+        </div>
+      </div>
+    }
+  />);
+});
