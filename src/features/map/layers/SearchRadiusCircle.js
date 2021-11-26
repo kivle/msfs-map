996d86fee27c691eab319507a0369363df71e7c3
Helge Elvik <helge.elvik@gmail.com>
2021-11-26 15:50:02 +0100

    Fix import path

--- src/features/map/layers/SearchRadiusCircle.js
+++ src/features/map/layers/SearchRadiusCircle.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Circle } from 'react-leaflet';
 import { useSelector } from 'react-redux';
 import { geolibToArrayPoint } from '../../../utils/geo';
-import { selectSearchCenterPoint, selectSearchRadius } from '../wikipedia/wikipediaSlice';
+import { selectSearchCenterPoint, selectSearchRadius } from '../../wikipedia/wikipediaSlice';
 
 export default function SearchRadiusCircle() {
   const searchRadius = useSelector(selectSearchRadius);
